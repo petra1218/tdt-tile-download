8f1957a0b036bd27fbe01c18848b758aee451e1c
petra <petra1218@gmail.com>
2015-09-09 00:31:28 +0800

    modify

--- src/com/petra/tdttiledownload/MainActivity.java
+++ src/com/petra/tdttiledownload/MainActivity.java
@@ -1,23 +1,28 @@
 package com.petra.tdttiledownload;
 
-import org.json.JSONArray;
+import java.io.IOException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.gson.JsonObject;
 import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
 import com.lidroid.xutils.http.ResponseStream;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 
 public class MainActivity extends Activity {
+	String LOG_TAG = MainActivity.class.getSimpleName();
+
 	@ViewInject(R.id.start_download)
 	Button startDownload;
 
@@ -25,13 +30,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		ViewUtils.inject(this);
 	}
 
 	@OnClick({ R.id.start_download, })
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.start_download:
+			DownloadThread downloadThread = new DownloadThread();
+			downloadThread.start();
 			break;
 		}
 
@@ -43,26 +50,60 @@ public class MainActivity extends Activity {
 		@Override
 		public void run() {
 			HttpUtils httpUtils = new HttpUtils();
-
+			RequestParams params = new RequestParams();
+			params.addBodyParameter(
+					"jsonStr",
+					"{\"keyWord\":\"海淀区\",\"level\":\"11\",\"mapBound\":\"-180,-90,180,90\",\"queryType\":\"1\",\"count\":\"10\",\"start\":\"0\"}");
+			// String url =
+			// "http://api.tianditu.com/api/api-new/query.do?jsonStr={\"keyWord\":\"海淀区\",\"level\":\"11\",\"mapBound\":\"-180,-90,180,90\",\"queryType\":\"1\",\"count\":\"10\",\"start\":\"0\"}";
+			String url = "http://api.tianditu.com/api/api-new/query.do?";
 			try {
-				responseStream = httpUtils
-						.sendSync(
-								HttpMethod.GET,
-								"http://api.tianditu.com/api/api-new/query.do?jsonStr={%22keyWord%22:%22%E5%A4%AA%E5%92%8C%E5%8C%BA%22,%22level%22:%2211%22,%22mapBound%22:%22-180,-90,180,90%22,%22queryType%22:%221%22,%22count%22:%2210%22,%22start%22:%220%22}");
-				JSONObject json = new JSONObject(responseStream.toString());
+				responseStream = httpUtils.sendSync(HttpMethod.POST, url,
+						params);
+				String responseStr = responseStream.readString();
+				Log.v(LOG_TAG, "response string = " + responseStr);
+				JSONObject json = new JSONObject(responseStr);
 				JSONObject area = json.optJSONObject("area");
 
 				String boundStr = area.optString("bound");
 
 				String[] boundArray = boundStr.split(",");
+				Log.v(LOG_TAG,
+						boundStr
+								+ String.format("{%s, %s, %s, %s}",
+										boundArray[0], boundArray[1],
+										boundArray[2], boundArray[3]));
 				double xMin = Double.parseDouble(boundArray[0]);
 				double xMax = Double.parseDouble(boundArray[2]);
 				double yMin = Double.parseDouble(boundArray[1]);
 				double yMax = Double.parseDouble(boundArray[3]);
+				Log.v(LOG_TAG, String.format("bound double = {%f,%f,%f,%f}",
+						xMin, yMin, xMax, yMax));
+
+				double[] tlMercator = TileUtils.lonLat2Mercator(xMin, yMin);
+				double[] trMercator = TileUtils.lonLat2Mercator(xMax, yMin);
+				double[] blMercator = TileUtils.lonLat2Mercator(xMin, yMax);
+				double[] brMercator = TileUtils.lonLat2Mercator(xMax, yMax);
+				Log.v(LOG_TAG, String.format(
+						"bound mercator = {%f,%f},{%f,%f},{%f,%f},{%f,%f}",
+						tlMercator[0], tlMercator[1], trMercator[0],
+						trMercator[1], blMercator[0], blMercator[1],
+						brMercator[0], brMercator[1]));
+
+				int[] tlRcNum = TileUtils.getRowAndColume(tlMercator, 15);
+				int[] trRcNum = TileUtils.getRowAndColume(trMercator, 15);
+				int[] blRcNum = TileUtils.getRowAndColume(blMercator, 15);
+				int[] brRcNum = TileUtils.getRowAndColume(brMercator, 15);
+				Log.v(LOG_TAG, String.format(
+						"bound mercator = {%d,%d},{%d,%d},{%d,%d},{%d,%d}",
+						tlRcNum[0], tlRcNum[1], trRcNum[0], trRcNum[1],
+						blRcNum[0], blRcNum[1], brRcNum[0], brRcNum[1]));
 
 			} catch (HttpException e) {
 				e.printStackTrace();
 			} catch (JSONException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
--- src/com/petra/tdttiledownload/TileUtils.java
+++ src/com/petra/tdttiledownload/TileUtils.java
@@ -1,48 +1,46 @@
-
-package com.mapzone.zmn.util;
+package com.petra.tdttiledownload;
 
 public class TileUtils {
 
-    String URL = "http://t1.tianditu.cn/DataServer?T=%s";
-    // URL = "http://t1.tianditu.cn/DataServer?T=img_w&X=843&Y=388&l=10"
+	static String URL = "http://t1.tianditu.cn/DataServer?T=%s";
+	// URL = "http://t1.tianditu.cn/DataServer?T=img_w&X=843&Y=388&l=10"
 
-    String baseImgName = "img_w&X=%d&Y=%d&l=%d";
-    String baseCiaName = "cia_w&X=%d&Y=%d&l=%d";
+	static String baseImgName = "img_w&X=%d&Y=%d&l=%d";
+	String baseCiaName = "cia_w&X=%d&Y=%d&l=%d";
 
-    String baseTilePath = "/home/petra/python/tile/beijing/%d/%s.map";
-    String path = "e:/tmp/cacheMap/img_w&X=843&Y=388&l=10.map";
+	static String baseTilePath = "/home/petra/python/tile/beijing/%d/%s.map";
+	String path = "e:/tmp/cacheMap/img_w&X=843&Y=388&l=10.map";
 
-    // 每张瓦片的像素
-    int pixelPerTile = 256;
+	// 每张瓦片的像素
+	static int pixelPerTile = 256;
 
-    // 实际上是0级时，墨卡托的整体长度显示到一张瓦片上，瓦片的像素是256*256
-    // 基础分辨率就是米每像素。
-    double baseResolution = 20037508.3427892 * 2 / pixelPerTile;
+	// 实际上是0级时，墨卡托的整体长度显示到一张瓦片上，瓦片的像素是256*256
+	// 基础分辨率就是米每像素。
+	static double baseResolution = 20037508.3427892 * 2 / pixelPerTile;
 
-    // 墨卡托原点（-20037508.3427892， 20037508.3427892）
-    double[] origin = {
-            -20037508.3427892, 20037508.3427892
-    };
+	// 墨卡托原点（-20037508.3427892， 20037508.3427892）
+	static double[] origin = { -20037508.3427892, 20037508.3427892 };
 
-    // 计算瓦片的行列号，返回下载瓦片的行列号
-    public String getRowAndColume(double[] xy, int level)
-    {
-        // '输入墨卡托坐标x，y，和需要计算的级别，返回包含当前坐标的瓦片的行列号'
-        double currResolution = baseResolution / Math.pow(2, level);
-        double meterPerTile = currResolution * pixelPerTile;
-        double colume = Math.ceil((xy[0] - origin[0]) / meterPerTile);
-        double row = Math.ceil(origin[1] - xy[1]) / meterPerTile;
-        String tilename = String.format(baseImgName, colume, row, level);
-        return String.format(URL, String.format(baseTilePath, level, tilename));
-    }
+	// 计算瓦片的行列号，返回下载瓦片的行列号
+	static public int[] getRowAndColume(double[] xy, int level) {
+		// '输入墨卡托坐标x，y，和需要计算的级别，返回包含当前坐标的瓦片的行列号'
+		double currResolution = baseResolution / Math.pow(2, level);
+		double meterPerTile = currResolution * pixelPerTile;
+		int colume = (int) Math.ceil((xy[0] - origin[0]) / meterPerTile);
+		int row = (int) Math.ceil((origin[1] - xy[1]) / meterPerTile);
+		return new int[] { row, colume };
+	}
 
-    public double[] lonLat2Mercator(double lon, double lat)
-    {
-        double x = lon * 20037508.342789 / 180;
-        double y = Math.log(Math.tan((90 + lat) * Math.PI / 360)) / (Math.PI / 180);
-        y *= 20037508.34789 / 180;
-        return new double[] {
-                x, y
-        };
-    }
+	private static String makeTileUrl(int level, double colume, double row) {
+		String tilename = String.format(baseImgName, colume, row, level);
+		return String.format(URL, String.format(baseTilePath, level, tilename));
+	}
+
+	static public double[] lonLat2Mercator(double lon, double lat) {
+		double x = lon * 20037508.342789 / 180;
+		double y = Math.log(Math.tan((90 + lat) * Math.PI / 360))
+				/ (Math.PI / 180);
+		y *= 20037508.34789 / 180;
+		return new double[] { x, y };
+	}
 }
