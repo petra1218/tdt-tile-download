a4d54c6a4fd5f1d82be84dcac083a8b68bd131c6
petra <petra1218@gmail.com>
2015-09-10 18:36:44 +0800

    增加影像下载功能，已经可以下载，有崩溃，无负载。

--- src/com/petra/tdttiledownload/MainActivity.java
+++ src/com/petra/tdttiledownload/MainActivity.java
@@ -1,18 +1,11 @@
+
 package com.petra.tdttiledownload;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import com.lidroid.xutils.HttpUtils;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.exception.HttpException;
@@ -21,102 +14,263 @@ import com.lidroid.xutils.http.ResponseStream;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
-	String LOG_TAG = MainActivity.class.getSimpleName();
+    String LOG_TAG = MainActivity.class.getSimpleName();
 
-	@ViewInject(R.id.start_download)
-	Button startDownload;
+    @ViewInject(R.id.start_download)
+    Button startDownload;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		ViewUtils.inject(this);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewUtils.inject(this);
+    }
 
-	@OnClick({ R.id.start_download, })
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.start_download:
-			DownloadThread downloadThread = new DownloadThread();
-			downloadThread.start();
-			break;
-		}
+    @OnClick({
+            R.id.start_download,
+    })
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_download:
+                DownloadThread downloadThread = new DownloadThread();
+                downloadThread.start();
+                break;
+        }
+    }
 
-	}
+    class DownloadThread extends Thread {
+        private static final int CACHE_SIZE = 30 * 1024;
+        private ResponseStream responseStream;
 
-	class DownloadThread extends Thread {
-		private ResponseStream responseStream;
+        @Override
+        public void run() {
+            HttpUtils httpUtils = new HttpUtils();
+            RequestParams params = new RequestParams();
+            params.addBodyParameter(
+                    "jsonStr",
+                    "{\"keyWord\":\"石景山区\",\"level\":\"11\",\"mapBound\":\"-180,-90,180,90\",\"queryType\":\"1\",\"count\":\"10\",\"start\":\"0\"}");
+            // String url =
+            // "http://api.tianditu.com/api/api-new/query.do?jsonStr={\"keyWord\":\"海淀区\",\"level\":\"11\",\"mapBound\":\"-180,-90,180,90\",\"queryType\":\"1\",\"count\":\"10\",\"start\":\"0\"}";
+            String url = "http://api.tianditu.com/api/api-new/query.do?";
+            try {
+                double left = 0, bottom = 0, right = 0, top = 0;
 
-		@Override
-		public void run() {
-			HttpUtils httpUtils = new HttpUtils();
-			RequestParams params = new RequestParams();
-			params.addBodyParameter(
-					"jsonStr",
-					"{\"keyWord\":\"海淀区\",\"level\":\"11\",\"mapBound\":\"-180,-90,180,90\",\"queryType\":\"1\",\"count\":\"10\",\"start\":\"0\"}");
-			// String url =
-			// "http://api.tianditu.com/api/api-new/query.do?jsonStr={\"keyWord\":\"海淀区\",\"level\":\"11\",\"mapBound\":\"-180,-90,180,90\",\"queryType\":\"1\",\"count\":\"10\",\"start\":\"0\"}";
-			String url = "http://api.tianditu.com/api/api-new/query.do?";
-			try {
-				responseStream = httpUtils.sendSync(HttpMethod.POST, url,
-						params);
-				String responseStr = responseStream.readString();
-				Log.v(LOG_TAG, "response string = " + responseStr);
-				JSONObject json = new JSONObject(responseStr);
-				JSONObject area = json.optJSONObject("area");
+                responseStream = httpUtils.sendSync(HttpMethod.POST, url, params);
+                String responseStr = responseStream.readString();
+                Log.v(LOG_TAG, "response string = " + responseStr);
+                JSONObject json = new JSONObject(responseStr);
+                JSONObject area = json.optJSONObject("area");
 
-				String boundStr = area.optString("bound");
+                String boundStr = area.optString("bound");
+                if ("".equals(boundStr))
+                {// 如果没有明确的给出bound，要自己算
+                 // 返回的边界可能是个数组。我们默认算出多个地块的整体外接边界。
+                    JSONArray pointsArray = area.getJSONArray("points");
 
-				String[] boundArray = boundStr.split(",");
-				Log.v(LOG_TAG,
-						boundStr
-								+ String.format("{%s, %s, %s, %s}",
-										boundArray[0], boundArray[1],
-										boundArray[2], boundArray[3]));
-				double left = Double.parseDouble(boundArray[0]);
-				double bottom = Double.parseDouble(boundArray[1]);
-				double right = Double.parseDouble(boundArray[2]);
-				double top = Double.parseDouble(boundArray[3]);
-				Log.v(LOG_TAG, String.format("bound double = {%f,%f,%f,%f}",
-						left, bottom, right, top));
+                    double tmp = 0;
+                    for (int i = 0; i < pointsArray.length(); i++)
+                    {
+                        JSONObject regionObject = (JSONObject) pointsArray.get(i);
+                        String regionStr = regionObject.optString("region");
+                        String[] pointArray = regionStr.split(",");
 
-				// 存放每一级影像的四个角的瓦片号，左（最小列號）、上（是小行号）、右（最大列号）、下（最大行号）
-				Map<Integer, int[]> tileMap = new HashMap<Integer, int[]>();
-				long tileCount = 0;
-				for (int i = 5; i < 19; i++) {
-					int[] tileBound = new int[] {
-							TileUtils.getColumeNumBy84(left, i),
-							TileUtils.getRowNumBy84(top, i),
-							TileUtils.getColumeNumBy84(right, i),
-							TileUtils.getRowNumBy84(bottom, i), };
-					tileMap.put(i, tileBound);
-					int tileXCount = tileBound[2] - tileBound[0];
-					int tileYCount = tileBound[3] - tileBound[1];
-					int currLevelCount = (0 == tileXCount ? 1 : tileXCount + 1)
-							* (0 == tileYCount ? 1 : tileYCount + 1);
-					tileCount += currLevelCount;
+                        String[] point = pointArray[0].split(" ");
 
-					Log.v(LOG_TAG,
-							String.format(
-									"level = %d, tileBound = {%d, %d, %d, %d },currLevelCount = %d, tileCount = %d",
-									i, tileBound[0], tileBound[1],
-									tileBound[2], tileBound[3], currLevelCount,
-									tileCount));
-				}
-				// 需要分别下载影像瓦片和矢量瓦片
-				tileCount *= 2;
+                        left = right = Double.parseDouble(point[0]);
+                        top = bottom = Double.parseDouble(point[1]);
 
-			} catch (HttpException e) {
-				e.printStackTrace();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+                        for (int j = 2; j < pointArray.length; j++)
+                        {
+                            point = pointArray[j].split(" ");
+                            tmp = Double.parseDouble(point[0]);
+                            if (tmp < left)
+                            {
+                                left = tmp;
+                            }
+                            else if (tmp > right)
+                            {
+                                right = tmp;
+                            }
 
-			super.run();
-		}
-	}
+                            tmp = Double.parseDouble(point[1]);
+                            if (tmp < bottom)
+                            {
+                                bottom = tmp;
+                            }
+                            else if (tmp > top)
+                            {
+                                top = tmp;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    String[] boundArray = boundStr.split(",");
+                    Log.v(LOG_TAG,
+                            boundStr
+                                    + String.format("{%s, %s, %s, %s}",
+                                            boundArray[0], boundArray[1],
+                                            boundArray[2], boundArray[3]));
+                    left = Double.parseDouble(boundArray[0]);
+                    bottom = Double.parseDouble(boundArray[1]);
+                    right = Double.parseDouble(boundArray[2]);
+                    top = Double.parseDouble(boundArray[3]);
+                }
+
+                Log.v(LOG_TAG, String.format("bound double = {%f,%f,%f,%f}",
+                        left, top, right, bottom));
+                // 存放每一级影像的四个角的瓦片号，左（最小列號）、上（是小行号）、右（最大列号）、下（最大行号）
+                Map<Integer, long[]> tileMap = new HashMap<Integer, long[]>();
+                long tileCount = 0;
+                for (int i = 5; i < 19; i++) {
+                    long[] tileBound = new long[] {
+                            TileUtils.getColumeNumBy84(left, i),
+                            TileUtils.getRowNumBy84(top, i),
+                            TileUtils.getColumeNumBy84(right, i),
+                            TileUtils.getRowNumBy84(bottom, i),
+                    };
+                    tileMap.put(i, tileBound);
+                    long tileXCount = tileBound[2] - tileBound[0];
+                    long tileYCount = tileBound[3] - tileBound[1];
+                    long currLevelCount = (0 == tileXCount ? 1 : tileXCount + 1)
+                            * (0 == tileYCount ? 1 : tileYCount + 1);
+                    tileCount += currLevelCount;
+
+                    File levelDir = new File(TileUtils.makeDir(i));
+                    if (!levelDir.isDirectory())
+                    {
+                        levelDir.mkdirs();
+                    }
+
+                    Log.v(LOG_TAG,
+                            String.format(
+                                    "level = %d, tileBound = {%d, %d, %d, %d },currLevelCount = %d, tileCount = %d",
+                                    i, tileBound[0], tileBound[1],
+                                    tileBound[2], tileBound[3], currLevelCount,
+                                    tileCount));
+                }
+
+                String imgUrl;
+                String ciaUrl;
+                ResponseStream responseStream;
+                InputStream input;
+                FileOutputStream outPut;
+                int readSize = 0;
+                int fileLength = 0;
+                File ciaFile;
+                byte[] fileCache = new byte[CACHE_SIZE];
+                File imgFile;
+                long timeImgStart = 0, timeImgEnd = 0, timeCiaStart = 0, timeCiaEnd = 0;
+                // 需要分别下载影像瓦片和矢量瓦片
+                for (int level : tileMap.keySet())
+                {
+                    long[] tileBound = tileMap.get(level);
+                    for (long colume = tileBound[0]; colume <= tileBound[2]; colume++)
+                    {
+                        for (long row = tileBound[1]; row <= tileBound[3]; row++)
+                        {
+                            imgUrl = TileUtils.makeImgUrl(level, colume, row);
+                            ciaUrl = TileUtils.makeCiaUrl(level, colume, row);
+                            Log.v(LOG_TAG, "ImgUrl = " + imgUrl);
+                            Log.v(LOG_TAG, "CiaUrl = " + ciaUrl);
+
+                            do
+                            {
+                                timeImgStart = System.currentTimeMillis();
+                                imgFile = new File(TileUtils.makeImgFile(level, colume, row));
+                                Log.v(LOG_TAG, "imgFile = " + imgFile.getPath());
+                                if (imgFile.exists() && imgFile.isFile())
+                                {
+                                    break;
+                                }
+                                Log.v(LOG_TAG, "imgFile = " + imgFile.getPath());
+                                outPut = new FileOutputStream(imgFile);
+                                Log.v(LOG_TAG, "imgFile = " + imgFile.getPath());
+                                responseStream = httpUtils.sendSync(HttpMethod.GET, imgUrl);
+                                input = responseStream.getBaseStream();
+                                readSize = 0;
+                                fileLength = 0;
+
+                                while ((readSize = input.read(fileCache, fileLength, CACHE_SIZE)) > 0)
+                                {// 设置的缓存不够大了
+                                    outPut.write(fileCache, fileLength, readSize);
+                                    fileLength += readSize;
+                                }
+
+                                Log.v(LOG_TAG, String.format(
+                                        "imgFile Entity length = %d write Length = %d",
+                                        responseStream.getContentLength(), fileLength));
+
+                                outPut.flush();
+                                outPut.close();
+                                input.close();
+                                timeImgEnd = System.currentTimeMillis();
+                            } while (false);
+
+                            do
+                            {
+                                timeCiaStart = System.currentTimeMillis();
+                                ciaFile = new File(TileUtils.makeCiaFile(level, colume, row));
+                                Log.v(LOG_TAG, "ciaFile = " + ciaFile.getPath());
+                                if (ciaFile.exists() && ciaFile.isFile())
+                                {
+                                    break;
+                                }
+                                Log.v(LOG_TAG, "ciaFile = " + ciaFile.getPath());
+                                outPut = new FileOutputStream(ciaFile);
+                                Log.v(LOG_TAG, "ciaFile = " + ciaFile.getPath());
+
+                                responseStream = httpUtils.sendSync(HttpMethod.GET, ciaUrl);
+                                input = responseStream.getBaseStream();
+
+                                readSize = 0;
+                                fileLength = 0;
+
+                                while ((readSize = input.read(fileCache, fileLength, CACHE_SIZE)) > 0)
+                                {// 设置的缓存不够大了
+                                    outPut.write(fileCache, fileLength, readSize);
+                                    fileLength += readSize;
+                                }
+                                Log.v(LOG_TAG, String.format(
+                                        "ciaFile Entity length = %d write Length = %d",
+                                        responseStream.getContentLength(), fileLength));
+
+                                outPut.flush();
+                                outPut.close();
+                                input.close();
+                                timeCiaEnd = System.currentTimeMillis();
+                            } while (false);
+
+                            long timeImg = timeImgEnd - timeImgStart;
+                            long timeCia = timeCiaEnd - timeCiaStart;
+                            Log.v(LOG_TAG, String.format(
+                                    "each tile Time(%d) = ImgTime(%d) + CiaTime(%d)", timeImg
+                                            + timeCia, timeImg, timeCia));
+                        }
+                    }
+                }
+
+            } catch (HttpException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            super.run();
+        }
+    }
 }
--- src/com/petra/tdttiledownload/TileUtils.java
+++ src/com/petra/tdttiledownload/TileUtils.java
@@ -1,59 +1,72 @@
+
 package com.petra.tdttiledownload;
 
 public class TileUtils {
 
-	static String URL = "http://t1.tianditu.cn/DataServer?T=%s";
-	// URL = "http://t1.tianditu.cn/DataServer?T=img_w&X=843&Y=388&l=10"
+    static String imgURL = "http://t1.tianditu.cn/DataServer?T=img_w&X=%d&Y=%d&l=%d";
+    static String ciaURL = "http://t1.tianditu.cn/DataServer?T=cia_w&X=%d&Y=%d&l=%d";
+    // URL = "http://t1.tianditu.cn/DataServer?T=img_w&X=843&Y=388&l=10"
 
-	static String baseImgName = "img_w&X=%d&Y=%d&l=%d";
-	String baseCiaName = "cia_w&X=%d&Y=%d&l=%d";
+    static String baseDir = "/sdcard/zmn/cache/%d";
+    static String imgFilePath = "/sdcard/zmn/cache/%d/img_w&X=%d&Y=%d&z=%d.map";
+    static String ciaFilePath = "/sdcard/zmn/cache/%d/cia_w&X=%d&Y=%d&z=%d.map";
 
-	static String baseTilePath = "/home/petra/python/tile/beijing/%d/%s.map";
-	String path = "e:/tmp/cacheMap/img_w&X=843&Y=388&l=10.map";
+    // 每张瓦片的像素
+    static int pixelPerTile = 256;
 
-	// 每张瓦片的像素
-	static int pixelPerTile = 256;
+    // 实际上是0级时，墨卡托的整体长度显示到一张瓦片上，瓦片的像素是256*256
+    // 基础分辨率就是米每像素。
+    static double baseResolution = 20037508.3427892 * 2 / pixelPerTile;
 
-	// 实际上是0级时，墨卡托的整体长度显示到一张瓦片上，瓦片的像素是256*256
-	// 基础分辨率就是米每像素。
-	static double baseResolution = 20037508.3427892 * 2 / pixelPerTile;
+    // 墨卡托原点（-20037508.3427892， 20037508.3427892）
+    static double[] origin = {
+            -20037508.3427892, 20037508.3427892
+    };
 
-	// 墨卡托原点（-20037508.3427892， 20037508.3427892）
-	static double[] origin = { -20037508.3427892, 20037508.3427892 };
+    // 计算瓦片的列号, 传入WGS84的经度
+    static public int getColumeNumBy84(double lon, int level) {
+        double meterPerTile = getMeterPerTile(level);
+        double x = lon * 20037508.342789 / 180;
+        return (int) Math.floor((x - origin[0]) / meterPerTile);
+    }
 
-	// 计算瓦片的列号, 传入WGS84的经度
-	static public int getColumeNumBy84(double x, int level) {
-		double meterPerTile = getMeterPerTile(level);
-		return (int) Math.ceil((lon2Mercator(x) - origin[0]) / meterPerTile);
-	}
+    // 计算瓦片的行号, 传入WGS84的纬度
+    static public int getRowNumBy84(double lat, int level) {
+        double meterPerTile = getMeterPerTile(level);
+        double y = Math.log(Math.tan((90 + lat) * Math.PI / 360))
+                / (Math.PI / 180);
+        y *= 20037508.34789 / 180;
+        return (int) Math.floor((origin[1] - y) / meterPerTile);
+    }
 
-	// 计算瓦片的行号, 传入WGS84的纬度
-	static public int getRowNumBy84(double y, int level) {
-		double meterPerTile = getMeterPerTile(level);
-		return (int) Math.ceil((origin[1] - lat2Mercator(y)) / meterPerTile);
-	}
+    private static double getMeterPerTile(int level) {
+        double currResolution = baseResolution / Math.pow(2, level);
+        double meterPerTile = currResolution * pixelPerTile;
+        return meterPerTile;
+    }
 
-	private static double getMeterPerTile(int level) {
-		double currResolution = baseResolution / Math.pow(2, level);
-		double meterPerTile = currResolution * pixelPerTile;
-		return meterPerTile;
-	}
+    public static String makeImgUrl(int level, long colume, long row) {
+        return String.format(imgURL, colume, row, level);
 
-	private static String makeTileUrl(int level, int colume, int row) {
-		String tilename = String.format(baseImgName, colume, row, level);
-		return String.format(URL, String.format(baseTilePath, level, tilename));
-	}
+    }
 
-	// WSG84经度转Mercator横坐标
-	static double lon2Mercator(double lon) {
-		return lon * 20037508.342789 / 180;
-	}
+    public static String makeCiaUrl(int level, long colume, long row) {
+        return String.format(ciaURL, colume, row, level);
+    }
 
-	// WSG84纬度转Mercator纵坐标
-	static double lat2Mercator(double lat) {
-		double y = Math.log(Math.tan((90 + lat) * Math.PI / 360))
-				/ (Math.PI / 180);
-		y *= 20037508.34789 / 180;
-		return y;
-	}
+    public static String makeImgFile(int level, long colume, long row)
+    {
+        return String.format(imgFilePath, level, colume, row, level);
+    }
+
+    public static String makeCiaFile(int level, long colume, long row)
+    {
+        return String.format(ciaFilePath, level, colume, row, level);
+    }
+
+    public static String makeDir(int level)
+    {
+        return String.format(baseDir, level);
+    }
+
 }
